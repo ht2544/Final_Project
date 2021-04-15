90ee453cdb64cf67a844da29ad1156e54d761aba
shikair2 <ruanshikai98@gmail.com>
2021-04-15 12:46:33 +0800

    clean views.py

--- map/views.py
+++ map/views.py
@@ -5,7 +5,6 @@ def index(request):
     squirrels = Squirrel.objects.all()[:100]
     context = {
             'squirrels': squirrels,
-            }
+    }
 
     return render(request, "map/index.html", context)
-# Create your views here.
